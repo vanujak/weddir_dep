8f44bbc8587159bc0da007689e5c8bf76d29f651
Vanuja Karunaratne <vanujakofficial@gmail.com>
2025-11-13 23:37:12 +0530

    Deploy up

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,17 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const corsOptions: CorsOptions = {
-    origin: ['http://localhost:3000', 'https://sayido.lk','https://sayido-eta.vercel.app','https://returning-wealth-sql-definition.trycloudflare.com'], // Allow only your Next.js app to access the API
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Allow cookies to be sent with requests
-    optionsSuccessStatus: 204, // Some legacy browsers choke on 204
+    origin: [
+      'http://localhost:3000',
+      'https://sayido.lk',
+      'https://sayido-eta.vercel.app',
+      'https://returning-wealth-sql-definition.trycloudflare.com',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization, X-Requested-With',
+    optionsSuccessStatus: 204,
   };
 
   app.enableCors(corsOptions);
   await app.listen(4000);
 }
+
 bootstrap();
